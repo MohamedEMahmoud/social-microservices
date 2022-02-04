980dadc73726bca2d95289788de72118d60fd2f9
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-02-04 11:52:41 +0200

    Updates

--- common/src/middlewares/validate-request.ts
+++ common/src/middlewares/validate-request.ts
@@ -6,15 +6,14 @@ export const validateRequest = (req: Request, res: Response, next: NextFunction)
 
     const passwordSpecialCharsValidation = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 
-    const fieldRequired = ['username', 'email', 'password', 'gender'];
-    const fieldName = [...Object.keys(req.body)];
-    for (let i in fieldRequired) {
-        if (!fieldName.includes(fieldRequired[i])) {
-            if ((fieldRequired[i] === 'username' && req.body.username.length < 8) || (fieldRequired[i] === 'password' && req.body.password.length < 8)) {
-                throw new BadRequestError(`Field ${fieldRequired[i]} must be more than 8 characters`);
+    const fields = ['username', 'email', 'password', 'gender'];
+    fields.map(field => {
+        if (field in req.body) {
+            if ((field === 'username' && req.body.username.length < 8) || (field === 'password' && req.body.password.length < 8)) {
+                throw new BadRequestError(`Field ${field} must be more than 8 characters`);
             }
-            if ((fieldRequired[i] === 'username' && /\s/gi.test(req.body.username)) || (!emailFormValidation.test(req.body.email))) {
-                throw new BadRequestError(`Invalid ${fieldRequired[i]}`);
+            if ((field === 'username' && /\s/gi.test(req.body.username)) || (!emailFormValidation.test(req.body.email))) {
+                throw new BadRequestError(`Invalid ${field}`);
             }
 
             if (!passwordSpecialCharsValidation.test(req.body.password)) {
@@ -24,9 +23,10 @@ export const validateRequest = (req: Request, res: Response, next: NextFunction)
             if (req.body.password.toLowerCase().includes("password") || req.body.password.toLowerCase().includes("qwerty") || req.body.password.toLowerCase().includes("asdf")) {
                 throw new BadRequestError("For security reasons! The Password can contain neither 'password' nor 'qwerty' nor 'asdf'.");
             }
-            throw new BadRequestError(`Field ${fieldRequired[i]} is required`);
+            throw new BadRequestError(`Field ${field} is required`);
         }
-    }
 
+    });
     next();
 };
+
