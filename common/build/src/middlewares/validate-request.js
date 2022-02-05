954f5424d033d91c715eed2867ca41ced4be9f1c
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-02-05 14:03:00 +0200

    Updates

--- common/build/src/middlewares/validate-request.js
+++ common/build/src/middlewares/validate-request.js
@@ -5,15 +5,14 @@ const bad_request_error_1 = require("../errors/bad-request-error");
 const validateRequest = (req, res, next) => {
     const emailFormValidation = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const passwordSpecialCharsValidation = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-    const fieldRequired = ['username', 'email', 'password', 'gender'];
-    const fieldName = [...Object.keys(req.body)];
-    for (let i in fieldRequired) {
-        if (!fieldName.includes(fieldRequired[i])) {
-            if ((fieldRequired[i] === 'username' && req.body.username.length < 8) || (fieldRequired[i] === 'password' && req.body.password.length < 8)) {
-                throw new bad_request_error_1.BadRequestError(`Field ${fieldRequired[i]} must be more than 8 characters`);
+    const fields = ['username', 'email', 'password', 'gender'];
+    fields.map(field => {
+        if (field in req.body) {
+            if ((field === 'username' && req.body.username.length < 8) || (field === 'password' && req.body.password.length < 8)) {
+                throw new bad_request_error_1.BadRequestError(`Field ${field} must be more than 8 characters`);
             }
-            if ((fieldRequired[i] === 'username' && /\s/gi.test(req.body.username)) || (!emailFormValidation.test(req.body.email))) {
-                throw new bad_request_error_1.BadRequestError(`Invalid ${fieldRequired[i]}`);
+            if ((field === 'username' && /\s/gi.test(req.body.username)) || (!emailFormValidation.test(req.body.email))) {
+                throw new bad_request_error_1.BadRequestError(`Invalid ${field}`);
             }
             if (!passwordSpecialCharsValidation.test(req.body.password)) {
                 throw new bad_request_error_1.BadRequestError("Password must contain a special character");
@@ -21,9 +20,9 @@ const validateRequest = (req, res, next) => {
             if (req.body.password.toLowerCase().includes("password") || req.body.password.toLowerCase().includes("qwerty") || req.body.password.toLowerCase().includes("asdf")) {
                 throw new bad_request_error_1.BadRequestError("For security reasons! The Password can contain neither 'password' nor 'qwerty' nor 'asdf'.");
             }
-            throw new bad_request_error_1.BadRequestError(`Field ${fieldRequired[i]} is required`);
+            throw new bad_request_error_1.BadRequestError(`Field ${field} is required`);
         }
-    }
+    });
     next();
 };
 exports.validateRequest = validateRequest;
--- common/src/middlewares/upload-files.ts
+++ common/src/middlewares/upload-files.ts
@@ -9,28 +9,19 @@ const upload = multer({ storage });
 const uploadFiles = (req: Request, res: Response, next: NextFunction) => {
 
     const files = req.files as { [fieldname: string]: Express.Multer.File[]; };
-
-    if (files.profilePicture) {
-        files.profilePicture!.map(file => {
-            if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-                throw new BadRequestError(`${file.originalname} should be valid image`);
-            }
-            if (file.size > 1e6) {
-                throw new BadRequestError(`${file.originalname} is larger`);
-            }
-        });
-    }
-
-    if (files.coverPicture) {
-        files.coverPicture.map(file => {
-            if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-                throw new BadRequestError(`${file.originalname} should be valid image`);
-            }
-            if (file.size > 1e6) {
-                throw new BadRequestError(`${file.originalname} is larger`);
-            }
-        });
-    }
+    const fields = ["profilePicture", "coverPicture", "image"];
+    fields.map(field =>{
+        if (field in files) {
+            files[field].map(file => {
+                if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+                    throw new BadRequestError(`${file.originalname} should be valid image`);
+                }
+                if (file.size > 1e6) {
+                    throw new BadRequestError(`${file.originalname} is larger`);
+                }
+            });
+        }
+    })
     next();
 };
 
