4818e8a1a9bb8082ba998d58a4d89f207484a75f
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-02-16 06:39:59 +0200

    create instance from UnfollowCreatedPublisher

--- auth/src/routes/unfollow-user.ts
+++ auth/src/routes/unfollow-user.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
-import { requireAuth, BadRequestError } from "@mesocial/common";
+import { requireAuth, BadRequestError, currentUser } from "@mesocial/common";
 import { User } from "../models/user.model";
+import { UnFollowCreatedPublisher } from "../events/publishers/unfollow-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -17,6 +19,14 @@ router.patch("/api/auth/user/unfollow",
         const currentUser = await User.findByIdAndUpdate(req.currentUser!.id, { $pull: { followings: user.id } }, { new: true });
 
         await user.updateOne({ $pull: { followers: currentUser!.id } });
+
+        await new UnFollowCreatedPublisher(natsWrapper.client).publish({
+            id: currentUser!.id,
+            follower: currentUser!.id,
+            currentUserVersion: currentUser!.version,
+            following: user.id,
+            userVersion: user.version
+        });
 
         res.status(200).send({ status: 200, currentUser, success: true });
     });
