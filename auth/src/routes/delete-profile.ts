096ad5200605f2fa93dba057e80a3e989d33db5e
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-02-16 06:36:37 +0200

    create instance from UserDeletedPublisher

--- auth/src/routes/delete-profile.ts
+++ auth/src/routes/delete-profile.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import { BadRequestError, requireAuth } from "@mesocial/common";
 import { User } from "../models/user.model";
+import { UserDeletedPublisher } from "../events/publishers/user-deleted-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -12,6 +14,9 @@ router.delete("/api/auth/user",
             throw new BadRequestError("Invalid credentials");
         }
         req.session = null;
+        await new UserDeletedPublisher(natsWrapper.client).publish({
+            id: user.id,
+        });
         res.status(204).send({});
     });
 
