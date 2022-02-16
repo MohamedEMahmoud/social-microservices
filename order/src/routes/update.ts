11d8ae5cc7e5cef2735e94d53537488b13418d15
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-02-16 07:33:05 +0200

    change userId keyword to buyerId and create instance from OrderCancelledPublisher

--- order/src/routes/update.ts
+++ order/src/routes/update.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import { requireAuth, BadRequestError, NotAuthorizedError } from "@mesocial/common";
 import mongoose from "mongoose";
 import { Order, OrderStatus } from "../models/order.model";
+import { natsWrapper } from "../nats-wrapper";
+import { OrderCancelledPublisher } from "../events/publishers/order-cancelled-publisher";
 const router = express.Router();
 
 router.patch("/api/order", requireAuth, async (req: Request, res: Response) => {
@@ -20,13 +22,19 @@ router.patch("/api/order", requireAuth, async (req: Request, res: Response) => {
         throw new BadRequestError("Order Not Found");
     }
 
-    if (order.userId !== req.currentUser!.id) {
+    if (order.buyerId !== req.currentUser!.id) {
         throw new NotAuthorizedError();
     }
 
     order.status = OrderStatus.cancelled;
 
     await order.save();
+
+    await new OrderCancelledPublisher(natsWrapper.client).publish({
+        id: order.id,
+        version: order.version,
+        product: order.product
+    });
 
     res.status(200).send({ status: 200, order, success: true });
 
