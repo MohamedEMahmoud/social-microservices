3467eff27d87ffc5d4fd882bf891427d12513bc6
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-02-16 06:13:03 +0200

    create nats-wrapper class to handle client and connect method

---
+++ auth/src/nats-wrapper.ts
@@ -0,0 +1,23 @@
+import nats, { Stan } from "node-nats-streaming";
+
+class NatsWrapper {
+    private _client?: Stan;
+    get client() {
+        if (!this._client) {
+            throw new Error("Can Not Access NATS Client Before Connecting From Auth Service");
+        }
+        return this._client;
+    }
+    connect(clusterId: string, clientId: string, url: string): Promise<void> {
+        this._client = nats.connect(clusterId, clientId, { url });
+        return new Promise((resolve, reject) => {
+            this._client!.on('connect', () => {
+                console.log("Connected To NATS From Auth Service");
+                resolve();
+            });
+            this._client!.on('error', (err) => reject(err));
+        });
+    }
+}
+
+export const natsWrapper = new NatsWrapper();
