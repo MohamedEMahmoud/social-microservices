cda9a63cbe08b0d98bde1f1579d014aab2ce853c
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-02-16 07:27:37 +0200

    Create ProductUpdatedListener to listen on ProductUpdatedPublisher

---
+++ order/src/events/listeners/product-updated-listener.ts
@@ -0,0 +1,28 @@
+import { Listener, ProductUpdatedEvent, Subjects } from "@mesocial/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+import { Product } from "../../models/product.model";
+
+export class ProductUpdatedListener extends Listener<ProductUpdatedEvent>{
+    readonly subject = Subjects.ProductUpdated;
+    queueGroupName = queueGroupName;
+    async onMessage(data: ProductUpdatedEvent["data"], msg: Message) {
+
+        const product = await Product.findById(data.id);
+
+        if (!product) {
+            throw new Error("Product Not Found");
+        }
+
+        product.set({
+            images: data.images,
+            content: data.content,
+            price: data.price
+        });
+
+        await product.save();
+
+        msg.ack();
+
+    }
+}
