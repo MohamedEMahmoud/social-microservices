17934e4b132f6ceaa5cb3a5dbc16229a16d9fb3e
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-02-16 06:32:50 +0200

    change isAdmin keyword to roles, check on query id by isValid method and create instance from BanCreatedPublisher

--- auth/src/routes/admin-ban-users.ts
+++ auth/src/routes/admin-ban-users.ts
@@ -3,6 +3,8 @@ import { User } from "../models/user.model";
 import { requireAuth, BadRequestError, upload } from "@mesocial/common";
 import moment from "moment";
 import mongoose from "mongoose";
+import { BanCreatedPublisher } from "../events/publishers/ban-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 const router = express.Router();
 
 interface BanUser {
@@ -19,11 +21,11 @@ router.patch("/api/auth/admin/ban",
 
         const user = await User.findById(req.currentUser!.id);
 
-        if (!user?.isAdmin) {
+        if (user?.roles !== "admin") {
             throw new BadRequestError("User have no this permission");
         }
-
-        if (!req.query.id || !mongoose.Types.ObjectId.isValid(String(req.query.id))) {
+        const { isValid } = mongoose.Types.ObjectId;
+        if (!req.query.id || !isValid(String(req.query.id))) {
             throw new BadRequestError("Id Is Invalid");
         }
 
@@ -33,7 +35,7 @@ router.patch("/api/auth/admin/ban",
             throw new BadRequestError("User no longer exists");
         }
 
-        if (existingUser.isAdmin) {
+        if (existingUser.roles === "admin") {
             throw new BadRequestError("Admin User");
         }
 
@@ -59,6 +61,18 @@ router.patch("/api/auth/admin/ban",
         existingUser.ban.push(ban);
 
         await existingUser.save();
+
+        const { id, end_in } = existingUser.ban[existingUser.ban.length - 1];
+
+        if (end_in) {
+            await new BanCreatedPublisher(natsWrapper.client).publish({
+                id: existingUser.id,
+                ban: {
+                    id,
+                    end_in
+                }
+            });
+        }
 
         res.status(200).send({ status: 200, existingUser, success: true });
 
