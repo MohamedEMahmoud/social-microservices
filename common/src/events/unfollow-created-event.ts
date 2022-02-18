caab27af27337b6b493ea2349091d03ae4f30bfd
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-02-18 21:29:04 +0200

    Updates

--- common/src/events/unfollow-created-event.ts
+++ common/src/events/unfollow-created-event.ts
@@ -3,7 +3,6 @@ import { Subjects } from "./subjects";
 export interface UnFollowCreatedEvent {
     subject: Subjects.UserUnFollow;
     data: {
-        id: string;
         follower: string;
         following: string;
         currentUserVersion: number;
