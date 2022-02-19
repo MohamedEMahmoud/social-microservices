aa41b78b3fef4bc6f786e489de39fd539d9b92cb
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-02-19 11:41:38 +0200

    Updates

--- common/build/src/events/subjects.js
+++ common/build/src/events/subjects.js
@@ -19,7 +19,6 @@ var Subjects;
     Subjects["ReplyDeleted"] = "reply:deleted";
     Subjects["ProductCreated"] = "product:created";
     Subjects["ProductUpdated"] = "product:updated";
-    Subjects["ProductDeleted"] = "product:deleted";
     Subjects["OrderCreated"] = "order:created";
     Subjects["OrderCancelled"] = "order:cancelled";
     Subjects["PaymentCreated"] = "payment:created";
